ceb9b6c4214a99fe2d800134d314fb5d5790cf55
ashwini <ashwinidivakar8@gmail.com>
2022-06-29 13:29:59 +0100

    reading data from json

---
+++ src/main/java/org/example/Book.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Book {
+    String Title;
+    String Author;
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String Title) {
+        this.Title = Title;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public void setAuthor(String Author) {
+        this.Author = Author;
+    }
+}
--- src/main/java/org/example/Books.java
+++ src/main/java/org/example/Books.java
@@ -1,15 +1,73 @@
 package org.example;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.introspect.VisibilityChecker;
+import java.io.*;
+import java.util.List;
 
 public class Books {
 
-    public void displayBookDetails(){
+
+    public void displayBookDetails()  {
 
         System.out.println("---------Available books are-------------");
+//        JSONParser jsonParser = new JSONParser();
+
+        try {
+            ObjectMapper mapper=new ObjectMapper();
+            mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+            mapper.setVisibility(VisibilityChecker.Std.defaultInstance().withFieldVisibility(JsonAutoDetect.Visibility.ANY));
+            InputStream inputStream = new FileInputStream(new File("src/main/resources/books_data.json"));
+            TypeReference<List<Book>> typeReference = new TypeReference<List<Book>>() {};
+            List<Book> books=mapper.readValue(inputStream,typeReference);
+            for (Book b:books){
+                System.out.println("Book Title is---- "+b.getTitle()+" Author is--- "+b.getAuthor());
+            }
+
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            throw new RuntimeException(e);
+        } catch (JsonParseException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        TypeReference<List<Book>> typeReference = new TypeReference<List<Book>>() {
+        };
+//        ***************************************
+//        JSONArray jsonarray = new JSONArray();
+//        for (int i = 0; i < jsonarray.length(); i++) {
+//            JSONObject jsonobject = jsonarray.getJSONObject(i);
+//            String title = jsonobject.getString("Title");
+//            String author = jsonobject.getString("Author");
+//            System.out.println(title+author);
+//        }
+//        **********************************************
+//        try (FileReader reader = new FileReader("books_data.json"))
+//        {
+//            //Read JSON file
+//            Object obj = jsonParser.parse(reader);
+//
+//            JSONArray employeeList = (JSONArray) obj;
+//            System.out.println(employeeList);
+//
+//            //Iterate over employee array
+//            employeeList.forEach( book -> parseBookObject( (JSONObject) book ) );
+//
+//        }catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+
 
 //        ***********************working csv file*************************
 //
